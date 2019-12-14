ba7f45153552cd3a6a55f05d3a21d5a4cde91960
Adel <kovacsadel12@gmail.com>
2019-12-14 10:38:01 +0100

    constants

---
+++ js-numerals/src/utils/constants.js
@@ -0,0 +1,59 @@
+export const TO_19 = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen"
+];
+
+export const TENS = [
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety"
+];
+
+export const DENOM = [
+    "",
+    "thousand",
+    "million",
+    "billion",
+    "trillion",
+    "quadrillion",
+    "quintillion",
+    "sextillion",
+    "septillion",
+    "octillion",
+    "nonillion",
+    "decillion",
+    "undecillion",
+    "duodecillion",
+    "tredecillion",
+    "quattuordecillion",
+    "sexdecillion",
+    "septendecillion",
+    "octodecillion",
+    "novemdecillion",
+    "vigintillion"
+];
+export const HUNDRED = "hundred";
--- js-numerals/src/utils/converter.js
+++ js-numerals/src/utils/converter.js
@@ -1,3 +1,5 @@
+import * as constants from './constants';
+
 
 const convertNumeralToWord = number => {
 
