80bc95c4f263d2a7229ddf5f2bb37a6dae802588
Adel <kovacsadel12@gmail.com>
2019-12-14 11:07:32 +0100

    three digit grouping method

--- js-numerals/src/utils/converter.js
+++ js-numerals/src/utils/converter.js
@@ -3,7 +3,23 @@ import * as constants from './constants';
 
 const convertNumeralToWord = number => {
 
+    const groups = getThreeDigitGroups(number);
+    console.log(groups);
     return number.toString();
+};
+
+
+const getThreeDigitGroups = number => {
+    let threeDigitGroups = [];
+    const absNumber = Math.abs(number);
+    const numOfGroups = Math.ceil(absNumber.toString().length / 3);
+
+    for (let i = 1; i <= numOfGroups; i++) {
+        threeDigitGroups.push(absNumber % 1000);
+        absNumber = Math.floor(absNumber / 1000)
+    }
+
+    return threeDigitGroups;
 };
 
 export default convertNumeralToWord;
