b67cf59ac9a05d15785e1f6449f10f2184d896b6
Adel <kovacsadel12@gmail.com>
2019-12-14 16:13:49 +0100

    responsive form

--- js-numerals/src/components/App.js
+++ js-numerals/src/components/App.js
@@ -5,27 +5,32 @@ import convertNumeral from '../utils/converter'
 
 function App() {
 
-    const [numeral, setNumeral] = useState(0);
+    const [numeral, setNumeral] = useState('');
     const [resultWord, setResultWord] = useState();
 
     const onSubmit = e => {
         e.preventDefault();
+        if (!Number.isInteger(numeral)) {
+            setResultWord("Enter whole number")
+        }
         setResultWord(convertNumeral(numeral));
-        setNumeral(0)
+        setNumeral('')
     };
+
 
     return (
         <div className="container">
             <form onSubmit={onSubmit}>
-                <h3>Convert Numeral to Words</h3>
+                <h3 className='h3-header'>Convert Numeral to Words</h3>
                 <input
+                    step="1"
                     placeholder="Insert number"
                     onChange={(e) => setNumeral(e.target.value)}
                     value={numeral}
                     type="number"
                     className="numeralInput"
                 />
-                <button className={'submitButton'}>Convert!</button>
+                <button className='submitButton'>Convert!</button>
                 <p className='result'>{resultWord}</p>
             </form>
         </div>
