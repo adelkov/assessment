14cc6acc9a3ca2f9a0b767c4b98ed1eba1af6a00
Adel <kovacsadel12@gmail.com>
2019-12-15 19:01:48 +0100

    refact, readabilty

--- js-numerals/src/utils/converter.js
+++ js-numerals/src/utils/converter.js
@@ -4,31 +4,34 @@ import {getDigitAtDenom, length} from './utils'
 const convertNumeralToWord = number => {
     if (+number === 0) return constants.TO_19[0];
 
-    if (length(number) === 4 && getDigitAtDenom(number, 100) % 10 !== 0) {
+    if (isFourDigitAndIrregular(number)) {
         return convertIrregularFourDigit(number);
     }
 
-    let word = '';
-    let groups = getThreeDigitGroups(number);
-    groups.forEach((number, index) => {
-        let toAppend = '';
-        if (+number !== 0) {
-            toAppend += convertThreeDigit(number);
-            if (index !== 0) {
-                toAppend += ' ' + constants.DENOM[index];
-            }
-            if (word !== '') {
-                toAppend += ` ${constants.AND} `;
-            }
-        }
-        word = toAppend + word;
-    });
+    let threeDigitGroups = getThreeDigitGroups(number);
+    let word = concatWithFillingWords(threeDigitGroups);
 
     if (number < 0) {
-        word = `${constants.NEGATIVE} ${word}`;
+        word = constants.NEGATIVE + ' ' + word;
     }
+
     return word.trim();
 };
+
+const concatWithFillingWords = groups => (
+    groups.reduce((acc, number, index) => {
+        if (number === 0) return acc;
+        let toAppend = '';
+        toAppend += convertThreeDigit(number);
+        if (index !== 0) {
+            toAppend += ' ' + constants.DENOM[index];
+        }
+        if (acc !== '') {
+            toAppend += ' ' + constants.AND + ' ';
+        }
+        return toAppend + acc;
+    }, '')
+);
 
 
 const getThreeDigitGroups = number => {
@@ -45,7 +48,7 @@ const getThreeDigitGroups = number => {
 };
 
 const convertTwoDigit = number => {
-    if (+number === 0) return '';
+    if (number === 0) return '';
     if (length(number) === 1) return constants.TO_19[number];
     if (number % 10 === 0) return constants.TENS[number / 10];
 
@@ -59,27 +62,31 @@ const convertTwoDigit = number => {
 
 
 const convertThreeDigit = number => {
-    const numLength = length(number);
-    if (numLength === 1) return constants.TO_19[number];
-    if (numLength === 2) return convertTwoDigit(number);
+    if (length(number) === 1) return constants.TO_19[number];
+    if (length(number) === 2) return convertTwoDigit(number);
 
     let word = '';
     const hundred = getDigitAtDenom(number, 100);
 
-    word += `${constants.TO_19[hundred]} ${constants.HUNDRED} `;
+    word += constants.TO_19[hundred] + ' ' + constants.HUNDRED + ' ';
     word += convertTwoDigit(number - hundred * 100);
     return word;
 };
 
 const convertIrregularFourDigit = number => {
-    const hundred =  getDigitAtDenom(number, 100);
+    const hundred = getDigitAtDenom(number, 100);
     let word = '';
     word += convertTwoDigit(hundred) + " " + constants.HUNDRED;
     const unit = number - hundred * 100;
     if (unit) {
-        word += ` ${constants.AND} ${convertTwoDigit(unit)} `;
+        word += ' ' + constants.AND + ' ' + convertTwoDigit(unit) + ' ';
     }
     return word.trim();
+};
+
+const isFourDigitAndIrregular = number => {
+    return length(number) === 4
+        && getDigitAtDenom(number, 100) % 10 !== 0
 };
 
 export default convertNumeralToWord;
--- js-numerals/src/utils/converter.test.js
+++ js-numerals/src/utils/converter.test.js
@@ -6,6 +6,7 @@ describe("Convert number to word", () => {
     });
     test('One digit', () => {
         expect(convertToWord(3)).toBe('three');
+        expect(convertToWord(7)).toBe('seven');
     });
     test('Two digit till 20', () => {
         expect(convertToWord(11)).toBe('eleven');
@@ -14,29 +15,34 @@ describe("Convert number to word", () => {
     });
     test('Two digit non zero', () => {
         expect(convertToWord(54)).toBe('fifty-four');
-        expect(convertToWord(96)).toBe('ninety-six')
-        expect(convertToWord(33)).toBe('thirty-three')
+        expect(convertToWord(96)).toBe('ninety-six');
+        expect(convertToWord(33)).toBe('thirty-three');
     });
     test('Two digit with zero', () => {
         expect(convertToWord(20)).toBe('twenty');
         expect(convertToWord(40)).toBe('forty');
     });
     test('Three digit no zero', () => {
+        expect(convertToWord(111)).toBe('one hundred eleven');
         expect(convertToWord(123)).toBe('one hundred twenty-three');
         expect(convertToWord(999)).toBe('nine hundred ninety-nine')
     });
     test('Three digit with zero', () => {
         expect(convertToWord(100)).toBe('one hundred');
-        expect(convertToWord(507)).toBe('five hundred seven')
+        expect(convertToWord(507)).toBe('five hundred seven');
     });
-    test('Four digit no zero', () => {
+    test('Irregular four digit', () => {
         expect(convertToWord(9876)).toBe('ninety-eight hundred and seventy-six');
-        expect(convertToWord(1111)).toBe('eleven hundred and eleven')
+        expect(convertToWord(1111)).toBe('eleven hundred and eleven');
+        expect(convertToWord(2001)).toBe('two thousand and one');
+        expect(convertToWord(1999)).toBe('nineteen hundred and ninety-nine');
+        expect(convertToWord(1509)).toBe('fifteen hundred and nine');
+        expect(convertToWord(2500)).toBe('twenty-five hundred');
+        expect(convertToWord(9300)).toBe('ninety-three hundred');
     });
-    test('Four digit with zero', () => {
+    test('Regular four digit', () => {
         expect(convertToWord(1000)).toBe('one thousand');
         expect(convertToWord(1010)).toBe('one thousand and ten');
-        expect(convertToWord(9300)).toBe('ninety-three hundred');
     });
     test('Large number', () => {
         expect(convertToWord(9000000000000)).toBe('nine trillion');
@@ -51,13 +57,8 @@ describe("Convert number to word", () => {
         expect(convertToWord(9000001)).toBe('nine million and one');
     });
     test('Negative number', () => {
+        expect(convertToWord(-1)).toBe('negative one');
         expect(convertToWord(-10)).toBe('negative ten');
         expect(convertToWord(-9000)).toBe('negative nine thousand');
     });
-    test('irregular four digit', () => {
-        expect(convertToWord(2001)).toBe('two thousand and one');
-        expect(convertToWord(1999)).toBe('nineteen hundred and ninety-nine');
-        expect(convertToWord(1509)).toBe('fifteen hundred and nine');
-        expect(convertToWord(2500)).toBe('twenty-five hundred');
-    })
 });
