da22a3adaf175bb7bbbc8b140bf2be0586b18f0e
Adel <kovacsadel12@gmail.com>
2020-01-10 10:24:29 +0100

    activation: component, strikethrough design

--- js-users/src/views/UserList.jsx
+++ js-users/src/views/UserList.jsx
@@ -5,11 +5,11 @@ import {
 } from "react-router-dom";
 import {formatDate} from "../util/data";
 
+const displayActivate = status => status === 'active' ? 'LOCK' : 'ACTIVATE';
 
 const UserList = () => {
     let {page} = useParams();
-    let {users, loading, error} = useContext(UsersContext);
-
+    let {users, loading, error, toggleStatus} = useContext(UsersContext);
     if (loading) {
         return (
             <div>Loading..</div>
@@ -25,11 +25,16 @@ const UserList = () => {
         <table>
             <tbody>
             {users[page].map(user => (
-                <tr key={user.id}>
+                <tr key={user.id} className={`user--${user.status}`}>
                     <td>{user['last_name']}</td>
                     <td>{user["first_name"]}</td>
                     <td>{formatDate(user["created_at"])}</td>
+                    <td onClick={() => toggleStatus(user.id, user.status, page)}>
+                        {user.loading ? 'loading...' : displayActivate(user.status)}
+                    </td>
+                    {user.error && <td>{user.error}</td>}
                 </tr>
+
             ))}
             </tbody>
         </table>
